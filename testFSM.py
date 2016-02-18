beacde9f3f1cea58e9ee0a45f16b68e43d0c0476
SunnyDjinn <guiol.matt@gmail.com>
2016-02-18 07:32:11 -0500

    Created .gitignore

---
+++ testFSM.py
@@ -0,0 +1,54 @@
+
+from FSM import *
+
+print "Creating x as follows..."
+x = FSM(1, [])
+x.addTransition(1, 2, 'a')
+x.addTransition(1, 3, 'b')
+x.addTransition(2, 3, 'b')
+x.grantAcceptingState(3)
+#rint x.toString()
+
+k = FSM.kleene(x)
+
+print k.toString()
+#k.draw()
+
+dfsm = FSM.determinise(x)
+print dfsm.toString()
+dfsm.draw()
+
+exit()
+
+print "Creating y as follows..."
+y = FSM(4, [])
+y.addTransition(4, 5, 'c')
+y.addTransition(5, 6, 'd')
+y.addTransition(5, 6, 'c')
+y.grantAcceptingState(6)
+y.grantAcceptingState(4)
+print y.toString()
+
+print "Uniting x and y..."
+z = FSM.union(x, y)
+print z.toString()
+
+#z.draw()
+
+print "w = Concatenating x and y..."
+w = FSM.concat(x, y)
+print w.toString()
+
+#w.draw()
+
+
+deter = FSM.determinise(w)
+print "Determizing w"
+print deter.toString()
+deter.draw()
+
+print deter.match("a");
+print deter.match("b");
+print deter.match("aa");
+print deter.match("abcc");
+
